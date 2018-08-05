713ff0d8bb930d7980ccb081ca2017df92ffb91d
hutsol <bohdan.hutsol@onedome.com>
2018-08-06 00:53:33 +0300

    add new sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,77 +1,25 @@
-import "slick-carousel/slick/slick";
 import "bootstrap/js/dist/util";
 import "bootstrap/js/dist/modal";
 import "bootstrap/js/dist/scrollspy";
+import "bootstrap/js/dist/collapse";
 import "jquery-easing";
 
 
 
 $(document).ready(function () {
 
-  // BEGIN Sliders
-  $(".js-slider-testimonials").slick({
-    infinite: true,
-    speed: 600,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    dots: true,
-    arrows: false,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-
-    ]
-  });
-
-  $(".js-slider-technologies").slick({
-    infinite: true,
-    speed: 600,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    pauseOnHover: false,
-    dots: false,
-    arrows: false,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2
-        }
-      }
-
-    ]
-  });
-  // END Sliders
-
-
   // BEGIN Header
   $(function () {
     const Breakpoint = 1024;
     let $html = $('html');
 
-    const handleHeader = (event) => {
+    const handleHeader = event => {
       const $this = $(event.target) || $(event);
       const headerClass = '.js-m-header';
       const stickyClass = 'm-header_sticky';
 
       if (window.innerWidth >= Breakpoint) {
-        if ($this.scrollTop() > 515) {
+        if ($this.scrollTop() > 50) {
           $(headerClass).addClass(stickyClass);
         } else {
           $(headerClass).removeClass(stickyClass);
@@ -99,7 +47,20 @@ $(document).ready(function () {
       handleHeader(window);
     });
 
-    $(window).on('scroll', handleHeader);
+    const handleBody = event => {
+      const $this = $(event.target) || $(event);
+
+      if ($this.scrollTop() > 50) {
+        $('body').addClass('is-scrolled')
+      } else {
+        $('body').removeClass('is-scrolled');
+      }
+    };
+
+    $(window).on('scroll', event => {
+      handleHeader(event);
+      handleBody(event);
+    });
   });
   // END Header
 
