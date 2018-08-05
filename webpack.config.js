85710966ec5efbc4e3a4d39a01ba3a1db57daebe
hutsol <bohdan.hutsol@onedome.com>
2018-08-06 01:16:40 +0300

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -84,29 +84,7 @@ function createConfig(env) {
 					minimize: true
 				})
 		);
-    webpackConfig.optimization = {
-      minimize: true,
-        runtimeChunk: true,
-        splitChunks: {
-        chunks: "async",
-          minSize: 1000,
-          minChunks: 2,
-          maxAsyncRequests: 5,
-          maxInitialRequests: 3,
-          name: true,
-          cacheGroups: {
-        default: {
-            minChunks: 1,
-              priority: -20,
-              reuseExistingChunk: true,
-          },
-          vendors: {
-            test: /[\\/]node_modules[\\/]/,
-              priority: -10
-          }
-        }
-      }
-    };
+    webpackConfig.optimization = {minimize: true};
 	}
 
 	return webpackConfig;
