6bb61fba149a95065154d8ac88db2bb72a5b2246
hutsol <bohdan.hutsol@onedome.com>
2018-08-08 19:47:12 +0300

    add blocks

--- src/js/app.js
+++ src/js/app.js
@@ -92,24 +92,26 @@ $(document).ready(function () {
   })(jQuery);
   // END Header scroll
 
-
-  function initMap() {
-    const mapContainer = document.querySelector('.contacts__map');
-
-    if (mapContainer) {
-      const izo = { lat: 50.4765582, lng: 30.503495816 };
-
-      const mapOptions = {
-        zoom: 15,
-        center: izo
-      };
-
-      const map = new google.maps.Map(mapContainer, mapOptions);
-
-      const marker = new google.maps.Marker({
-        position: izo,
-        map
-      })
-    }
-  }
 });
+
+const initMap = () => {
+  const mapContainer = document.querySelector('.contacts__map');
+
+  if (mapContainer) {
+    const izo = { lat: 50.4765582, lng: 30.503495816 };
+
+    const mapOptions = {
+      zoom: 15,
+      center: izo
+    };
+
+    const map = new google.maps.Map(mapContainer, mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: izo,
+      map
+    })
+  }
+};
+
+initMap();
